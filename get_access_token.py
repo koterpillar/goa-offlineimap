a951a2c43ccc2f0fa8d47f629fcc4b78c2d48e4d
Alexey Kotlyarov <a@koterpillar.com>
2016-08-07 20:22:41 +1000

    Get the client ID and secret too

--- get_access_token.py
+++ get_access_token.py
@@ -5,6 +5,8 @@ import sys
 
 import dbus
 
+OBJECT_MANAGER = 'org.freedesktop.DBus.ObjectManager'
+PROPERTIES = 'org.freedesktop.DBus.Properties'
 
 GOA_PATH = '/org/gnome/OnlineAccounts'
 GOA_NAME = 'org.gnome.OnlineAccounts'
@@ -12,24 +14,61 @@ GOA_MANAGER_PATH = '/org/gnome/OnlineAccounts/Manager'
 GOA_ACCOUNT = 'org.gnome.OnlineAccounts.Account'
 GOA_ACCOUNT_OAUTH2 = 'org.gnome.OnlineAccounts.OAuth2Based'
 
-bus = dbus.SessionBus()
 
-goa_manager = bus.get_object(GOA_NAME, GOA_PATH)
+def get_account(bus):
+    """Get the path to the only online account set up."""
 
-goa_objects = goa_manager.GetManagedObjects(dbus_interface='org.freedesktop.DBus.ObjectManager')
+    goa_manager = bus.get_object(GOA_NAME, GOA_PATH)
 
-accounts = [
-    obj for obj in goa_objects
-    if obj != GOA_MANAGER_PATH
-]
+    goa_objects = goa_manager.GetManagedObjects(dbus_interface=OBJECT_MANAGER)
 
-if len(accounts) > 1:
-    sys.exit("More than one account found.")
+    accounts = [
+        obj for obj in goa_objects
+        if obj != GOA_MANAGER_PATH
+    ]
 
-(account_path,) = accounts
+    if len(accounts) > 1:
+        sys.exit("More than one account found.")
 
-account = bus.get_object(GOA_NAME, account_path)
+    (account_path,) = accounts
 
-account.EnsureCredentials(dbus_interface=GOA_ACCOUNT)
-access_token, _ = account.GetAccessToken(dbus_interface=GOA_ACCOUNT_OAUTH2)
-print(access_token)
+    return bus.get_object(GOA_NAME, account_path)
+
+
+def get_client_id(account):
+    """Get the client ID from the online account."""
+
+    return account.Get(GOA_ACCOUNT_OAUTH2, 'ClientId',
+                       dbus_interface=PROPERTIES)
+
+
+def get_client_secret(account):
+    """Get the client secret from the online account."""
+
+    return account.Get(GOA_ACCOUNT_OAUTH2, 'ClientSecret',
+                       dbus_interface=PROPERTIES)
+
+
+def get_access_token(account):
+    """Get the access token from the online account."""
+
+    account.EnsureCredentials(dbus_interface=GOA_ACCOUNT)
+    access_token, _ = account.GetAccessToken(dbus_interface=GOA_ACCOUNT_OAUTH2)
+    return access_token
+
+
+def main():
+    """Print the account credentials."""
+
+    bus = dbus.SessionBus()
+
+    account = get_account(bus)
+
+    print(
+        get_client_id(account),
+        get_client_secret(account),
+        get_access_token(account),
+    )
+
+if __name__ == '__main__':
+    main()
